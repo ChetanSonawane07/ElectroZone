5bf02a848b9c65625f90bfc3c437d2df43e0d399
omkshirsagar07 <omkshirsagar055@gmail.com>
2024-08-14 16:41:53 +0530

    mmmm

--- ElectroZone Frontend/src/componants/WishLIstItem.jsx
+++ ElectroZone Frontend/src/componants/WishLIstItem.jsx
@@ -1,35 +1,35 @@
-import { useDispatch } from "react-redux";
-import { removeFromCartAction } from "../features/cartSlice";
+// import { useDispatch } from "react-redux";
+// import { removeFromCartAction } from "../features/cartSlice";
 
-function WishListItem() {
-  // used to update the state
-  const dispatch = useDispatch();
+// function WishListItem() {
+//   // used to update the state
+//   const dispatch = useDispatch();
 
-  return (
-    <div className="col-md-4 mb-4">
-      <div className="card">
-        <img
-          src={imageSrc}
-          className="card-img-top"
-          alt={item.title}
-          style={{ height: '200px', objectFit: 'cover' }}
-        />
-        <div className="card-body">
-          <h4 className="card-title">{item.name}</h4>
-          <hr/>
-          <p className="card-text">{item.description}</p>
-          <div className="d-flex justify-content-between align-items-center">
-            <button
-              onClick={removeFromCart}
-              className="btn btn-danger"
-            >
-              Remove
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="col-md-4 mb-4">
+//       <div className="card">
+//         <img
+//           src={imageSrc}
+//           className="card-img-top"
+//           alt={item.title}
+//           style={{ height: '200px', objectFit: 'cover' }}
+//         />
+//         <div className="card-body">
+//           <h4 className="card-title">{item.name}</h4>
+//           <hr/>
+//           <p className="card-text">{item.description}</p>
+//           <div className="d-flex justify-content-between align-items-center">
+//             <button
+//               onClick={removeFromCart}
+//               className="btn btn-danger"
+//             >
+//               Remove
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default WishListItem;
+// export default WishListItem;
