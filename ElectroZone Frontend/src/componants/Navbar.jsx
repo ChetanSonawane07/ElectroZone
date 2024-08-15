b43244c51cb7df717fe0fe350bb8021a4f10c5b9
Chetan <chetan@gmail.com>
2024-08-16 00:03:34 +0530

    navbar changes

--- ElectroZone Frontend/src/componants/Navbar.jsx
+++ ElectroZone Frontend/src/componants/Navbar.jsx
@@ -26,31 +26,32 @@ function Navbar() {
           </a>
         </div>
         <span
-          className="nav-link text-white fw-bold btn"
+          className="nav-link me-4 text-white fw-bold btn"
           onClick={() => navigate('/AboutUs')}
           style={{ cursor: "pointer", marginRight: "10px" }}
         >
           About Us
         </span>
 
-        <button className="btn btn-outline-success" style={{ marginRight: 20 }} onClick={() => navigate('/Seller-Register')}>
+        <button className="btn me-3 btn-outline-success" onClick={() => navigate('/Seller-Register')}>
           Become a seller
         </button>
 
         <div className="d-flex align-items-center">
           <button
-            className="text-reset me-3 border-none btn"
+            className="text-reset border-none btn"
+            onClick={permission ? () => navigate('/Wishlist') : () => navigate('/User-Login')}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
+            </svg>
+          </button>
+          <button
+            className="text-reset me-2 border-none btn"
             onClick={permission ? () => navigate('/Cart') : () => navigate('/User-Login')}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="currentColor"
-              className="bi bi-bag-heart-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
             </svg>
           </button>
 
@@ -74,13 +75,13 @@ function Navbar() {
             </a>
 
             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-              {permission ? 
-              null : 
-              <li>
-                <button className="dropdown-item" onClick={() => navigate('/User-Login')}>
-                  Login
-                </button>
-              </li>}
+              {permission ?
+                null :
+                <li>
+                  <button className="dropdown-item" onClick={() => navigate('/User-Login')}>
+                    Login
+                  </button>
+                </li>}
               {permission && (
                 <>
                   <li>
